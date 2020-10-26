c3f500c56d6bcdd788df427889d07b8023c45cdb
jgindis <jgindis20@stuy.edu>
2020-10-25 20:29:30 -0400

    Accounted for an empty array as the input to method sum()

--- ArrayOps.java
+++ ArrayOps.java
@@ -5,6 +5,7 @@ public class ArrayOps {
   }
 
   public static int sum(int[] arr) {
+    if (arr.length == 0) return 0;
     int sum = 0;
     for (int pos = 0; pos < arr.length; pos++) {
       sum += arr[pos];
