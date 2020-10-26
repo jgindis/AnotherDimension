b38ec8bd10cf937ae3f0af2e63f464d3bba4ccc6
jgindis <jgindis20@stuy.edu>
2020-10-25 20:14:58 -0400

    Just testing

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,46 +1,5 @@
 public class ArrayOps {
 
-  public static void main(String[] args) {
 
-  }
-
-  public static int sum(int[] arr) {
-    return 0;
-  }
-
-  public static  int largest(int[]arr) {
-    return 0;
-  }
-
-  public static int[] sumRows(int[][] matrix) {
-    int[] a = new int[0];
-    return a;
-  }
-
-  public static  int[] largestInRows(int[][] matrix) {
-    int[] a = new int[0];
-    return a;
-  }
-
-  public static  int sum(int[][] arr) {
-    return 0;
-  }
-
-  public static int[] sumCols(int[][] matrix) {
-    int[] a = new int[0];
-    return a;
-  }
-
-  public static boolean isRowMagic(int[][] matrix) {
-    return false;
-  }
-
-  public static boolean isColMagic(int[][] matrix) {
-    return false;
-  }
-
-  public static boolean isLocationMagic(int[][] matrix, int row, int col) {
-    return false;
-  }
 
 }
