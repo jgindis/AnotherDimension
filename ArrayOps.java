86c05e22e5f197b479d41d641c2223fb66de4cca
jgindis <jgindis20@stuy.edu>
2020-10-26 22:52:40 -0400

    Added sumCols()

--- ArrayOps.java
+++ ArrayOps.java
@@ -70,7 +70,12 @@ public class ArrayOps {
   }
 
   public static int[] sumCols(int[][] matrix) {
-    int[] a = new int[0];
+    int a[] = new int[matrix[0].length];
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+      a[j] += matrix[i][j];
+      }
+    }
     return a;
   }
 
